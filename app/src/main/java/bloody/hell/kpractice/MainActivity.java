c971edb017e2994fcb13065e10afd0d4e35e1572
Mamert Stankiewicz <mamert@oke.pl>
2017-08-07 11:54:23 +0200

    build flavors

--- app/src/main/java/bloody/hell/kpractice/MainActivity.java
+++ app/src/main/java/bloody/hell/kpractice/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements MainFrag.OnFragme
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, R.string.greetingString, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
