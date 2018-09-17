6b3167750b7b28bdbcd150b05539d4b638378c06
Mamert Stankiewicz <mamert@oke.pl>
2018-09-17 13:42:01 +0200

    removed floatingactionbutton

--- app/src/main/java/bloody/hell/kpractice/MainActivity.java
+++ app/src/main/java/bloody/hell/kpractice/MainActivity.java
@@ -31,15 +31,6 @@ public class MainActivity extends AppCompatActivity implements MainFrag.OnFragme
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, R.string.greetingString, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         navigateTo(R.id.menu_item_main, true);
     }
 
