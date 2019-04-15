bf101b30bcafd346084e26d1639ad7e5fc69b9a3
Mamert Stankiewicz <mamert@oke.pl>
2019-04-15 10:26:05 +0200

    + net: methods to tell which net type we're using

--- app/src/main/java/bloody/hell/kpractice/utils/Net.java
+++ app/src/main/java/bloody/hell/kpractice/utils/Net.java
@@ -1,5 +1,11 @@
 package bloody.hell.kpractice.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.telephony.TelephonyManager;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -28,6 +34,60 @@ public class Net {
         if (urlc.getResponseCode() != 204 ||
                 urlc.getContentLength() != 0)
             throw new RuntimeException("Network N/A");
+    }
+
+    /**
+     * Checks whether the device is connected to the LTE network. Also checks if
+     * LTE is the active connection, not the WiFi.
+     * @return true if the device is connected to the LTE network.
+     */
+    public static boolean isLTEConnected(final Context context) {
+        boolean DEBUG = false;
+        if (!DEBUG) {
+            try {
+                ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                boolean notWifi = connectivity.getActiveNetworkInfo().getType() != ConnectivityManager.TYPE_WIFI;
+                TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                return notWifi && tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_LTE;
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    /** Checks whether WiFi connection is available.
+     *  @return true if the device is connected to wifi network.
+     */
+    public static boolean isWifiAvailable(final Context context) {
+        try {
+            WifiManager wm = (WifiManager) context.getApplicationContext()
+                    .getSystemService(Context.WIFI_SERVICE);
+            int netId = wm.getConnectionInfo().getNetworkId();
+            return netId != -1;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks whether 3G connection is available.
+     *
+     * @param context
+     * @return true if 3G connection is active.
+     */
+    public static boolean is3GConnected(final Context context) {
+        try {
+            ConnectivityManager connectivity = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo network = connectivity.getActiveNetworkInfo();
+            boolean notWifi = network.getType() != ConnectivityManager.TYPE_WIFI
+                    && network.isConnected();
+            return notWifi;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
